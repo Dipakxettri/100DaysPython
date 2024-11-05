0ac3db46560a8753d8bf48ef16a3c9b2170e9ef7
Dipakxettri <codeepakghimire@gmail.com>
2024-11-05 15:53:17 +0545

    update

---
+++ Day001Python/04variables.py
@@ -0,0 +1,1 @@
+a = 2; #here a is a variable(container) which stores value 2 with the use of assignment operator(=)
---
+++ Day001Python/05dataTypes.py
@@ -0,0 +1,17 @@
+#Some Data Types :
+
+#1.int
+a = 2
+
+#2.float
+b = 2.2
+
+#3.string
+c = "deepak"
+
+#4.boolean : stores true and false
+d = True
+
+#5.none
+e = None
+
