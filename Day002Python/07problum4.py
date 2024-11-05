710568137b6437d51c5ef885e634fbbd64e266cb
Dipakxettri <codeepakghimire@gmail.com>
2024-11-05 19:53:35 +0545

    update

---
+++ Day002Python/07problum4.py
@@ -0,0 +1,6 @@
+#checking whether the b is greater or not
+
+a = 20
+b = 80
+c = a < b
+print(c)
---
+++ Day002Python/08problum5.py
@@ -0,0 +1,10 @@
+
+# calculating average 
+a = 4
+b = 80
+c = 20
+d = 90
+totalNums = 4
+totalSum = a + b + c + d
+average = totalSum/totalNums
+print("The average is ",average)
